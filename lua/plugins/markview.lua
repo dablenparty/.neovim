aa217d4957f50a4b729c8cfc1f858f31395a297c
dablenparty <anoptimisticcouch@gmail.com>
2025-02-27 23:40:18 -0500

    feat(plugins)!: add markview, bullets
    Markdown and bulleted lists

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    'OXY2DEV/markview.nvim',
+    lazy = false,
+    opts = function(_, opts)
+      opts = opts or {}
+      local presets = require 'markview.presets'
+
+      return {
+        markdown = {
+          headings = presets.headings.glow,
+          horizontal_rules = presets.horizontal_rules.thick,
+          tables = presets.tables.rounded,
+        },
+      }
+    end,
+  },
+  {
+    'bullets-vim/bullets.vim',
+    ft = { 'gitcommit', 'markdown', 'text', 'scratch' },
+    config = function() end,
+  },
+}
