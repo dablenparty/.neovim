b0e28aa66bfeb8f58bba4ddeab4c22815df92c28
dablenparty <anoptimisticcouch@gmail.com>
2025-03-03 02:24:35 -0500

    fix(snacks): don't show LSP-related binds until LspAttach

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -417,58 +417,6 @@ return {
         end,
         desc = 'Colorschemes',
       },
-      -- TODO: move LSP binds to an autocommand
-      -- LSP
-      {
-        'gd',
-        function()
-          Snacks.picker.lsp_definitions()
-        end,
-        desc = 'Goto Definition',
-      },
-      {
-        'gD',
-        function()
-          Snacks.picker.lsp_declarations()
-        end,
-        desc = 'Goto Declaration',
-      },
-      {
-        'gr',
-        function()
-          Snacks.picker.lsp_references()
-        end,
-        nowait = true,
-        desc = 'References',
-      },
-      {
-        'gI',
-        function()
-          Snacks.picker.lsp_implementations()
-        end,
-        desc = 'Goto Implementation',
-      },
-      {
-        'gy',
-        function()
-          Snacks.picker.lsp_type_definitions()
-        end,
-        desc = 'Goto T[y]pe Definition',
-      },
-      {
-        '<leader>ss',
-        function()
-          Snacks.picker.lsp_symbols()
-        end,
-        desc = 'LSP Symbols',
-      },
-      {
-        '<leader>sS',
-        function()
-          Snacks.picker.lsp_workspace_symbols()
-        end,
-        desc = 'LSP Workspace Symbols',
-      },
     },
     init = function()
       vim.api.nvim_create_autocmd('User', {
@@ -492,6 +440,25 @@ return {
           Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map '<leader>uc'
           Snacks.toggle.treesitter():map '<leader>uT'
           Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(event)
+          local function set_key(keys, func, opts)
+            opts = opts or {}
+            vim.tbl_deep_extend('keep', opts, { buffer = event.buf })
+            opts.desc = 'LSP: ' .. opts.desc
+            vim.keymap.set('n', keys, func, opts)
+          end
+
+          set_key('gd', Snacks.picker.lsp_definitions, { desc = '[G]oto [D]efinition' })
+          set_key('gD', Snacks.picker.lsp_declarations, { desc = '[G]oto [D]eclaration' })
+          set_key('gr', Snacks.picker.lsp_references, { nowait = true, desc = '[G]oto [R]eferences' })
+          set_key('gI', Snacks.picker.lsp_implementations, { desc = '[G]oto [I]mplementation' })
+          set_key('gy', Snacks.picker.lsp_type_definitions, { desc = '[G]oto T[y]pe Definition' })
+          set_key('<leader>ss', Snacks.picker.lsp_symbols, { desc = 'LSP [S]ymbols' })
+          set_key('<leader>sS', Snacks.picker.lsp_workspace_symbols, { desc = 'LSP Workspace [S]ymbols' })
         end,
       })
     end,
