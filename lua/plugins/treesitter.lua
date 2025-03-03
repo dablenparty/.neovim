cde2b0bcc3d8e6314a925e032723f1c9588769bf
dablenparty <anoptimisticcouch@gmail.com>
2025-03-03 02:08:42 -0500

    refactor(trouble)!: make Diagnostic keybind group

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -107,9 +107,6 @@ return {
           enable = true,
           border = 'none',
           floating_preview_opts = { border = 'rounded' },
-          peek_definition_code = {
-            ['<leader>df'] = '@function.outer',
-          },
         },
       },
     },
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -64,7 +64,7 @@ return {
     -- Document existing key chains
     spec = {
       { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
-      { '<leader>d', group = '[D]ocument' },
+      { '<leader>d', group = '[D]iagnostics' },
       { '<leader>f', group = '[F]ind...' },
       { '<leader>g', group = '[G]it' },
       { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
