941b23df603ad581241fd534a7c5359e41224bb4
dablenparty <anoptimisticcouch@gmail.com>
2025-03-21 15:32:10 -0400

    feat(plugins): add patterns plugin
    It explains regex and lua patterns, with a match tester

---
+++ lua/plugins/patterns.lua
@@ -0,0 +1,16 @@
+return {
+  'OXY2DEV/patterns.nvim',
+  cmd = { 'Patterns' },
+  keys = {
+    {
+      '<C-k>',
+      '<cmd>Patterns hover<cr>',
+      desc = 'Show pattern hover',
+    },
+    {
+      '<C-e>',
+      '<cmd>Patterns explain<cr>',
+      desc = 'Explain pattern',
+    },
+  },
+}
