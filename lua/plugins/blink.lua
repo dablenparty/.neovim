963a69870480d06de069087dab1b666cc1ea4842
dablenparty <anoptimisticcouch@gmail.com>
2025-04-26 23:40:56 -0400

    refactor(blink): change cargo build syntax

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -32,11 +32,7 @@ return {
     dependencies = 'rafamadriz/friendly-snippets',
 
     -- Build from source with Rust nightly
-    build = 'cargo +nightly build --release',
-    -- The +toolchain syntax requires that cargo is installed with rustup.
-    -- Therefore, if you get build errors, make sure you're using the
-    -- right `cargo` binary. This is especially important if you use Homebrew
-    -- as it will always take precedence.
+    build = 'rustup run nightly cargo build',
 
     event = { 'InsertEnter', 'LspAttach' },
     lazy = true,
