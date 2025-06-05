bae9866812c704ad454643228852377a2b0551b5
dablenparty <anoptimisticcouch@gmail.com>
2025-06-04 23:39:37 -0400

    fix(blink): temporarily disable snippets
    For some reason, they're always appearing first and there's a LOTTT of
    them.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -68,7 +68,7 @@ return {
 
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
-        default = { 'lsp', 'buffer', 'snippets', 'path' },
+        default = { 'lsp', 'buffer', 'path' },
 
         providers = {
           lazydev = {
