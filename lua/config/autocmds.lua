2cce2ae0e05fea05eb6502d1586ab5142aa02562
dablenparty <anoptimisticcouch@gmail.com>
2025-07-13 16:14:14 -0400

    feat(autocmds): absolute line numbers for insert
    They're relative by default, but when entering Insert mode, they become
    absolute again

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -16,13 +16,14 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
   command = 'set statuscolumn=',
 })
 
+-- The following two autocommands are used to highlight references of the
+-- word under your cursor when your cursor rests there for a little while.
+--    See `:help CursorHold` for information about when this is executed
+-- When you move your cursor, the highlights will be cleared (the second autocommand).
+-- This only enables when LSP is supported.
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
   callback = function(event)
-    -- The following two autocommands are used to highlight references of the
-    -- word under your cursor when your cursor rests there for a little while.
-    --    See `:help CursorHold` for information about when this is executed
-    -- When you move your cursor, the highlights will be cleared (the second autocommand).
     local client = vim.lsp.get_client_by_id(event.data.client_id)
     if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
       local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
@@ -57,5 +58,34 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function()
     vim.hl.on_yank()
+  end,
+})
+
+-- Modified from: https://github.com/sitiom/nvim-numbertoggle/blob/main/plugin/numbertoggle.lua
+local relative_line_nums_augroup = vim.api.nvim_create_augroup('relative-line-numbers', { clear = true })
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave', 'CmdlineLeave', 'WinEnter' }, {
+  pattern = '*',
+  group = relative_line_nums_augroup,
+  callback = function()
+    -- if line numbers are enabled
+    if vim.o.nu then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufLeave', 'InsertEnter', 'CmdlineEnter', 'WinLeave' }, {
+  pattern = '*',
+  group = relative_line_nums_augroup,
+  callback = function()
+    if vim.o.nu then
+      vim.opt.relativenumber = false
+      -- Conditional taken from https://github.com/rockyzhang24/dotfiles/commit/03dd14b5d43f812661b88c4660c03d714132abcf
+      -- Workaround for https://github.com/neovim/neovim/issues/32068
+      if not vim.tbl_contains({ '@', '-' }, vim.v.event.cmdtype) then
+        vim.cmd 'redraw'
+      end
+    end
   end,
 })
