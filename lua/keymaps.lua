986b2c724e20ac169eb732f6e2e9f27530a391f3
dablenparty <anoptimisticcouch@gmail.com>
2025-07-17 17:16:39 -0400

    refactor(keymaps): export set_global_keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,28 +1,33 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+M = {}
+
+-- Set a global keymap. All args are passed directly to `vim.keymap.set()` except `opts`.
+-- If `opts.desc` is defined, it is ovewritten by the positional `desc`. If `opts.mode`
+-- is defined, it is removed from `opts` and used as the keymap mode.
 ---@param lhs  string
 ---@param rhs  string | function
 ---@param desc string
----@param opts vim.keymap.set.Opts | { mode?: string|string[] }
-function set_global_keymap(lhs, rhs, desc, opts)
-  local opts = opts or {}
+---@param opts vim.keymap.set.Opts | { mode?: string|string[] }?
+M.set_global_keymap = function(lhs, rhs, desc, opts)
+  opts = opts or {}
   local mode = table.removekey(opts, 'mode') or 'n'
   opts.desc = desc
   vim.keymap.set(mode, lhs, rhs, opts)
 end
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-set_global_keymap('<Esc>', ':nohlsearch<CR>', 'Clear search highlights')
+M.set_global_keymap('<Esc>', ':nohlsearch<CR>', 'Clear search highlights')
 
 -- Center screen when jumping
-set_global_keymap('n', 'nzzzv', 'Next search result (centered)')
-set_global_keymap('N', 'Nzzzv', 'Previous search result (centered)')
-set_global_keymap('<C-d>', '<C-d>zz', 'Half page down (centered)')
-set_global_keymap('<C-u>', '<C-u>zz', 'Half page up (centered)')
+M.set_global_keymap('n', 'nzzzv', 'Next search result (centered)')
+M.set_global_keymap('N', 'Nzzzv', 'Previous search result (centered)')
+M.set_global_keymap('<C-d>', '<C-d>zz', 'Half page down (centered)')
+M.set_global_keymap('<C-u>', '<C-u>zz', 'Half page up (centered)')
 
 -- Paste without yanking in visual mode
-set_global_keymap('p', 'P', 'Paste without yanking', { mode = 'x' })
-set_global_keymap('y%', function()
+M.set_global_keymap('p', 'P', 'Paste without yanking', { mode = 'x' })
+M.set_global_keymap('y%', function()
   local clipboard = vim.opt.clipboard:get()
   local register = ''
   if vim.tbl_contains(clipboard, 'unnamedplus') then
@@ -34,24 +39,26 @@ set_global_keymap('y%', function()
 end, 'Yank buffer', { expr = true })
 
 -- navigate windows easier
-set_global_keymap("<C-h>", "<C-w>h", "Move to left window")
-set_global_keymap("<C-j>", "<C-w>j", "Move to bottom window")
-set_global_keymap("<C-k>", "<C-w>k", "Move to top window")
-set_global_keymap("<C-l>", "<C-w>l", "Move to right window")
+M.set_global_keymap("<C-h>", "<C-w>h", "Move to left window")
+M.set_global_keymap("<C-j>", "<C-w>j", "Move to bottom window")
+M.set_global_keymap("<C-k>", "<C-w>k", "Move to top window")
+M.set_global_keymap("<C-l>", "<C-w>l", "Move to right window")
 
 -- NOTE: you can create splits with <C-W>s and <C-W>v
 -- Resize splits
-set_global_keymap("<C-Up>", ":resize +2<CR>", "Increase window height")
-set_global_keymap("<C-Down>", ":resize -2<CR>", "Decrease window height")
-set_global_keymap("<C-Left>", ":vertical resize -2<CR>", "Decrease window width")
-set_global_keymap("<C-Right>", ":vertical resize +2<CR>", "Increase window width")
+M.set_global_keymap("<C-Up>", ":resize +2<CR>", "Increase window height")
+M.set_global_keymap("<C-Down>", ":resize -2<CR>", "Decrease window height")
+M.set_global_keymap("<C-Left>", ":vertical resize -2<CR>", "Decrease window width")
+M.set_global_keymap("<C-Right>", ":vertical resize +2<CR>", "Increase window width")
 
 -- Move lines up/down
-set_global_keymap("<A-j>", ":m .+1<CR>==", "Move line down")
-set_global_keymap("<A-k>", ":m .-2<CR>==", "Move line up")
-set_global_keymap("<A-j>", ":m '>+1<CR>gv=gv", "Move selection down", { mode = 'v' })
-set_global_keymap("<A-k>", ":m '<-2<CR>gv=gv", "Move selection up", { mode = 'v' })
+M.set_global_keymap("<A-j>", ":m .+1<CR>==", "Move line down")
+M.set_global_keymap("<A-k>", ":m .-2<CR>==", "Move line up")
+M.set_global_keymap("<A-j>", ":m '>+1<CR>gv=gv", "Move selection down", { mode = 'v' })
+M.set_global_keymap("<A-k>", ":m '<-2<CR>gv=gv", "Move selection up", { mode = 'v' })
 
 -- VSCode indenting in visual mode
-set_global_keymap("<Tab>", ">gv", "Indent right and reselect", { mode = "v" })
-set_global_keymap("<S-Tab>", "<gv", "Indent left and reselect", { mode = "v" })
+M.set_global_keymap("<Tab>", ">gv", "Indent right and reselect", { mode = "v" })
+M.set_global_keymap("<S-Tab>", "<gv", "Indent left and reselect", { mode = "v" })
+
+return M
