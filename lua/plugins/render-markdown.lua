b1b692ea296bc3d664524c4acf94530bcdbbfcee
dablenparty <anoptimisticcouch@gmail.com>
2025-07-17 17:21:24 -0400

    feat(plugins): markdown viewing

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', { 'echasnovski/mini.icons', version = false, opts = {} } },
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ft = { 'markdown', 'quarto', 'text', 'gitcommit' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      completions = { lsp = { enabled = true } }
+    },
+  },
+  {
+    'bullets-vim/bullets.vim',
+    ft = { 'gitcommit', 'markdown', 'text', 'scratch' },
+    -- NOTE: opts won't work, bullets is VimScript
+    config = function() end
+  }
+}
