13451b2475d01def7331fc2438b4429945290d08
dablenparty <anoptimisticcouch@gmail.com>
2025-07-27 11:54:59 -0400

    refactor(obsidian)!: note_path_func
    It creates notes with the title as the filename and checks for illegal
    characters

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -37,6 +37,18 @@ return {
       folder = 'Templates',
     },
 
+    ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+    ---@return string|obsidian.Path The full path to the new note.
+    note_path_func = function(spec)
+      local illegal_chars = '*"\\/<>:|?'
+      local title = spec.title or spec.id
+      if title:match('[' .. illegal_chars .. ']') then
+        error(string.format("Note title '%s' cannot contain characters %s", title, illegal_chars))
+      end
+      local path = spec.dir / title
+      return path:with_suffix '.md'
+    end,
+
     wiki_link_func = 'prepend_note_path',
 
     picker = {
