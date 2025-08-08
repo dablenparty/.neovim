0944976408b96697f5a2ace329d00dd211ff1461
dablenparty <anoptimisticcouch@gmail.com>
2025-08-08 15:11:07 -0400

    feat(neogit): NeogitStandalone user command
    This allows me to launch Neogit from the command
    line and use it as a standalone git client. This
    is how I'm replacing lazygit.

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,5 +1,19 @@
+--- This is a convenience command for opening Neogit and setting `q` to quit from the status pane.
+--- It is mean to be used externally, i.e. by opening neovim with `nvim +NeogitStandalone`
+vim.api.nvim_create_user_command('NeogitStandalone', function(_)
+  require('neogit').open { kind = 'replace' }
+  local neogit_bufnr = vim.api.nvim_get_current_buf()
+  vim.keymap.set('n', 'q', '<cmd>quit<cr>', { buffer = neogit_bufnr, desc = 'Quit', nowait = true })
+end, { desc = 'Open Neogit as a Standalone Buffer, like :Man!' })
+
 return {
   'NeogitOrg/neogit',
+  cmd = {
+    'Neogit',
+    'NeogitCommit',
+    'NeogitLogCurrent',
+    'NeogitResetState',
+  },
   dependencies = {
     'nvim-lua/plenary.nvim',
     -- diffs
